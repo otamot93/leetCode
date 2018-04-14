d770f9af45b9fdf796d44f00575760d2385690f5
hezhichao <hezhichao93@163.com>
2018-04-14 15:50:28 +0800

    q26

---
+++ src/q27/Solution.java
@@ -0,0 +1,19 @@
+package q27;
+
+public class Solution {
+    public static void main(String[] args) {
+        int[] nums = new int[]{1,2,3,4,5};
+        int result = new Solution().removeElement(nums,2);
+        System.out.println(result);
+    }
+    public int removeElement(int[] nums, int val) {
+        int all = 0;
+        for (int i = 0; i <nums.length; i++) {
+            if(nums[i] != val){
+                nums[all] = nums[i];
+                all++;
+            }
+        }
+        return all;
+    }
+}
