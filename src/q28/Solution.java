794550c22be9f8d242bf6025e37779f241d3fcd2
hezhichao <hezhichao93@163.com>
2018-04-28 16:03:11 +0800

    q28

---
+++ src/q28/Solution.java
@@ -0,0 +1,43 @@
+package q28;
+
+public class Solution {
+    public static void main(String[] args) {
+        int result = new Solution().strStr("","issipi");
+        System.out.println(result);
+    }
+    public int strStr(String haystack, String needle) {
+        if(needle.length()==0){
+            return 0;
+        }
+        if(haystack.length() == 0){
+            return -1;
+        }
+        if(needle.length()>haystack.length()){
+            return -1;
+        }
+        byte[] hByte = haystack.getBytes();
+        byte[] nByte = needle.getBytes();
+        for (int i = 0; i <hByte.length ; i++) {
+            if(hByte.length-i <nByte.length){//剩下字符串比目标串要短
+                break;
+            }
+            if(hByte[i] == nByte[0]){//第一个字相同，则继续比较
+                int j = 1;
+                for(j = 1;j<nByte.length;j++){
+                    if((i+j)> hByte.length-1){
+                        break;
+                    }
+                    if(hByte[i+j] != nByte[j]){
+                        break;
+                    }
+                }
+                if(j == nByte.length){
+                    return i;
+                }
+            }
+        }
+
+        //找不到
+        return -1;
+    }
+}
